43467ecacb0c96201890b379429cb1959d2f936d
Petar Pofuk <ppofuk@gmail.com>
2016-03-15 17:26:46 +0100

    Use ~/autostart.sh in test.

--- xephyr_init.sh
+++ xephyr_init.sh
@@ -1,6 +1,7 @@
 Xephyr +extension RANDR -ac -screen 800x600 -br -noreset :1 &
 sleep 1
 export DISPLAY=:1.0
-xterm &
-xsetroot -cursor_name left_ptr &
-./bin/debug/nightwing
+# The following is in ~/autostart.sh
+# xterm &
+# xsetroot -cursor_name left_ptr &
+./bin/debug/nightwing -e ~/autostart.sh
