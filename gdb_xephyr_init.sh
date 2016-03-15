3f950d81a0ce4369369b11c92257d12b42291c1c
Petar Pofuk <ppofuk@gmail.com>
2016-03-15 17:28:23 +0100

    Init xephyr for debuging with gdb.

--- gdb_xephyr_init.sh
+++ gdb_xephyr_init.sh
@@ -1,1 +1,6 @@
-Xephyr +extension RANDR -ac -screen 800x600 -br -reset -terminate 2> /dev/null :1 & (sleep 1; export DISPLAY=:1; xterm & xsetroot -cursor_name left_ptr & gdb ./bin/debug/nightwing)
+Xephyr +extension RANDR -ac -screen 800x600 -br -noreset :1 &
+sleep 1
+export DISPLAY=:1.0
+xterm &
+xsetroot -cursor_name left_ptr &
+gdb ./bin/debug/nightwing
